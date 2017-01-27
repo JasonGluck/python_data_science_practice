f489f523360c804c9bc090fee2c2899a93b6795a
github username <github email address>
2017-01-27 13:25:54 -0500

    decile vertical bar

---
+++ decile_bar_chart.py
@@ -0,0 +1,17 @@
+from matplotlib import pyplot as plt
+from collections import Counter
+
+def bar_chart():
+    grades = [83,95,91,87,70,0,85,82,100,67,73,77,0]
+    decile = lambda grade: grade // 10 * 10
+    histogram = Counter(decile(grade) for grade in grades)
+
+    plt.bar([x for x in histogram.keys()], histogram.values(), 8)
+    plt.axis([-5,105,0,5])
+    plt.xticks([10 * i for i in range(11)])
+    plt.xlabel("Decile")
+    plt.ylabel("Number of Students")
+    plt.title("Distribution of Exam Grades")
+    plt.show()
+
+bar_chart()
